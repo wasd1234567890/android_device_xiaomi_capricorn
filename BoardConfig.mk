97b2304d3e43942aac5f5b925ca96289922ffed2
Alexander Koskovich <akoskovich@pm.me>
2022-03-02 13:41:26 -0700

    capricorn: Set USB product string to 'Xiaomi MI 5s'
    Change-Id: Idda062aaa38e16d7ba65356dbca8b467fbff14c1

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -30,5 +30,8 @@ DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 # Kernel
 TARGET_KERNEL_CONFIG += vendor/xiaomi/capricorn.config
 
+# Properties
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
+
 # inherit from the proprietary version
 -include vendor/xiaomi/capricorn/BoardConfigVendor.mk
