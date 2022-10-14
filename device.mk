670f38e9bbf42373bebc76a475a151789f6df5b3
Nicholas Magill <nmagill1979@gmail.com>
2022-10-14 14:09:09 +0100

    capricorn: Build vendor variant of biometrics HIDL
    Change-Id: Idae2bb5a5e6278bd2e3918ed98e08b371c4ef3a4

--- device.mk
+++ device.mk
@@ -36,7 +36,8 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1
+    android.hardware.biometrics.fingerprint@2.1 \
+    android.hardware.biometrics.fingerprint@2.1.vendor
 
 # FlipFlap
 PRODUCT_PACKAGES += \
