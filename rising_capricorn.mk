54f9f0b9ba9716609275d2ed9200500ddfcd7056
demonbro <52437128+wasd1234567890@users.noreply.github.com>
2023-11-18 19:00:16 +0800

    Update rising_capricorn.mk

--- rising_capricorn.mk
+++ rising_capricorn.mk
@@ -18,7 +18,7 @@
 $(call inherit-product, device/xiaomi/capricorn/device.mk)
 
 # Inherit some common RisingOS stuff.
-$(call inherit-product, vendor/rising/config/rising.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := rising_capricorn
